c7b7a44e37298315f9c333297955e4dd1aa50866
Allan Boite <allan.boite@gmail.com>
2020-02-19 08:31:37 +0100

    amélioration du code

--- model/manager.php
+++ model/manager.php
@@ -5,7 +5,7 @@ class Manager{
 		
 		// Connexion à la base de données
 
-			//$bdd = new PDO('mysql:host=db5000301926.hosting-data.io;dbname=dbs294977;charset=utf8', 'dbu519301', 'Spartrox117*');
+			
 			$bdd = new PDO('mysql:host=localhost;dbname=projet_p4;charset=utf8', 'root', '');
 			return $bdd;
 	}
